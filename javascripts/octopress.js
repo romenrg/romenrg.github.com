b5219cfc65fb8941728d2b5f142ca1aae1dd5bbf
Romén Rodríguez-Gil <rrodriguezgil@cloudbees.com>
2020-09-24 15:25:14 +0100

    Site updated at 2020-09-24 14:25:14 UTC

--- javascripts/octopress.js
+++ javascripts/octopress.js
@@ -11,7 +11,6 @@ function getNav() {
   mobileNav.children('select').bind('change', function(event) {
     if (event.target.value) { window.location.href = event.target.value; }
   });
-  mobileNav.find('select').val('');
 }
 
 function addSidebarToggler() {
